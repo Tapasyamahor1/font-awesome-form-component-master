b98687b715f84c35593c7ef8b3b151375f51f5e3
Tapasyamahor1 <136682540+Tapasyamahor1@users.noreply.github.com>
2023-06-15 12:49:40 +0530

    Create FontAwesomeIconModalDialogViewModel.cs

---
+++ FontAwesomeFormComponent/Models/ModalDialogs/FontAwesomeIconModalDialogViewModel.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace NetC.FontAwesomeFormComponent.Kentico.MVC
+{
+    public class FontAwesomeIconModalDialogViewModel
+    {
+        public IEnumerable<FontAwesomeIconSelectorListItem> Icons { get; set; }
+    }
+}
