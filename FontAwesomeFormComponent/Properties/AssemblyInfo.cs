b4a8f2978432a2fd422bdec24ed687a5b0aaf3fa
Tapasyamahor1 <136682540+Tapasyamahor1@users.noreply.github.com>
2023-06-15 12:29:03 +0530

    Create AssemblyInfo.cs

---
+++ FontAwesomeFormComponent/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using CMS;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Font Awesome Icon Form Component")]
+[assembly: AssemblyDescription("Adds the ability for a content editor to select a Font Awesome 5.x icon from a modal popup.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NetConstruct")]
+[assembly: AssemblyProduct("NetC.FontAwesomeFormComponent")]
+[assembly: AssemblyCopyright("MIT")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDiscoverable]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("93134b79-4d52-4b49-8a18-cfba584bf2c2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("12.29.4.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
